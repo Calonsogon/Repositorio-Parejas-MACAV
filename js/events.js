7a7bc4023a89d152688e75263f68de2b7c4e05cf
Valentina Toni <vale.toni93@gmail.com>
2024-05-30 10:11:29 +0200

    completed styling pop-up

--- js/events.js
+++ js/events.js
@@ -6,8 +6,12 @@ import { initializeGame } from './functions.js';
 const urlParams = new URLSearchParams(window.location.search);
 const level = urlParams.get('level');
 
-
-
 document.addEventListener('DOMContentLoaded', initializeGame(level, levels));
+
+const tempButton = document.querySelector('.popup-toggle');
+const popup = document.querySelector('dialog');
+tempButton.addEventListener('click', () => {
+    popup.showModal();
+});
 
 
