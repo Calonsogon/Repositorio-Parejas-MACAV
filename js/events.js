309ec3746c2c159ad3169c78f32fe3b2225e5f2f
Valentina Toni <vale.toni93@gmail.com>
2024-05-30 11:47:00 +0200

    bug-fixed

--- js/events.js
+++ js/events.js
@@ -1,26 +1,26 @@
 import { levels } from './levels.js';
-import { initializeGame, startCountdown } from './functions.js';
+import { initializeGame, startCountdown, toggleSound, flipCard } from './functions.js';
 
-import { endGame, increasePoints, updatePointsDisplay, toogleSound } from "./functions.js";//no entiendo pq increa.. y end.. no se enlazan
+// import { endGame, increasePoints, updatePointsDisplay, toogleSound } from "./functions.js";//no entiendo pq increa.. y end.. no se enlazan
 
 const urlParams = new URLSearchParams(window.location.search);
 const level = urlParams.get('level');
 
-//DOM cargado
 document.addEventListener('DOMContentLoaded', () => {
     initializeGame(level, levels);
-    const twoMinutes = 60 * 2;
-    startCountdown(twoMinutes);
-});
+    const time = 60 * 2; 
+    startCountdown(time)
 
-//click cartas
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", flipCard);
-});
+    let cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            flipCard(levels, e.currentTarget);
+        })
+    }
+    )
+}
+);
 
-//const card = document.querySelector('.card');
-
-//botón reinicio listener
 const restartButton = document.querySelector(".restart-button");
 restartButton.addEventListener("click", () => {
     clearInterval(countdownInterval);
@@ -29,14 +29,9 @@ restartButton.addEventListener("click", () => {
     startCountdown(twoMinutes);
 });
 
-//Toggle sonido
 const soundToggle = document.getElementById("sound-toggle");
-soundToggle.addEventListener("change", toogleSound);
+soundToggle.addEventListener("change", toggleSound);
 
-
-
-
-//card.addEventListener('click', flipCard());
 
 
 
